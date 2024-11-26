6d7a704b2ed0d4a692cb7482b3591d2b1421d246
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-26 09:54:08 +0330

    [ADD] added User model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from accounts.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User Model for our app
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
