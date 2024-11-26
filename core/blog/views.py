1ffc543b4f9daeea15b64c6d30d05c2768d13d3c
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-26 16:40:56 +0330

    [ADD] add Comment

--- core/blog/views.py
+++ core/blog/views.py
@@ -5,6 +5,7 @@ from django.views import View
 from blog.forms import PostCreateUpdateForm, CommentCreateForm, CommentReplyForm
 from blog.models import Post
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -54,3 +55,64 @@ class PostDetailView(View):
             new_comment.save()
             messages.success(request, 'your comment submitted successfully', 'success')
             return redirect('blog:post_detail', self.post_instance.id)
+
+class PostAddReplyView(View):
+	form_class = CommentReplyForm
+
+	def post(self, request, post_id, comment_id):
+		post = get_object_or_404(Post, id=post_id)
+		comment = get_object_or_404(Comment, id=comment_id)
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			reply = form.save(commit=False)
+			reply.user = request.user
+			reply.post = post
+			reply.reply = comment
+			reply.is_reply = True
+			reply.save()
+			messages.success(request, 'your reply submitted successfully', 'success')
+		return redirect('home:post_detail', post.id, post.slug)
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+	def get(self, request, post_id):
+		post = get_object_or_404(Post, pk=post_id)
+		if post.user.id == request.user.id:
+			post.delete()
+			messages.success(request, 'post deleted successfully', 'success')
+		else:
+			messages.error(request, 'you cant delete this post', 'danger')
+		return redirect('home:home')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+	form_class = PostCreateUpdateForm
+
+	def setup(self, request, *args, **kwargs):
+		self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'])
+		return super().setup(request, *args, **kwargs)
+
+	def dispatch(self, request, *args, **kwargs):
+		post = self.post_instance
+		if not post.user.id == request.user.id:
+			messages.error(request, 'you cant update this post', 'danger')
+			return redirect('home:home')
+		return super().dispatch(request, *args, **kwargs)
+
+	def get(self, request, *args, **kwargs):
+		post = self.post_instance
+		form = self.form_class(instance=post)
+		return render(request, 'home/update.html', {'form':form})
+
+	def post(self, request, *args, **kwargs):
+		post = self.post_instance
+		form = self.form_class(request.POST, instance=post)
+		if form.is_valid():
+			new_post = form.save(commit=False)
+			new_post.slug = slugify(form.cleaned_data['body'][:30])
+			new_post.save()
+			messages.success(request, 'you updated this post', 'success')
+			return redirect('home:post_detail', post.id, post.slug)
+
+
+
