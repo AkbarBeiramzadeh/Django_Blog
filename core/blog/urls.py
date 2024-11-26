a208fb93329979d2e4f88c36d89b80c38a4741d0
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-26 16:29:38 +0330

    [WIP]

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='post-list'),
     path('post/detail/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/create/', views.PostCreateView.as_view(), name='post_create'),
+    path('reply/<int:post_id>/<int:comment_id>/', views.PostAddReplyView.as_view(), name='add_reply'),
+    path('post/delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/update/<int:post_id>/', views.PostUpdateView.as_view(), name='post_update'),
+
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView
 from django.views import View
 
-from blog.forms import PostCreateUpdateForm
+from blog.forms import PostCreateUpdateForm, CommentCreateForm, CommentReplyForm
 from blog.models import Post
 from django.contrib import messages
 
@@ -31,21 +31,18 @@ class PostCreateView(View):
 
 
 class PostDetailView(View):
-    # form_class = CommentCreateForm
-    # form_class_reply = CommentReplyForm
+    form_class = CommentCreateForm
+    form_class_reply = CommentReplyForm
 
     def setup(self, request, *args, **kwargs):
-        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'])
+        self.post_instance = get_object_or_404(Post, pk=kwargs['pk'])
         return super().setup(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         comments = self.post_instance.pcomments.filter(is_reply=False)
-        can_like = False
-        if request.user.is_authenticated and self.post_instance.user_can_like(request.user):
-            can_like = True
         return render(request, 'blog/post_detail.html',
                       {'post': self.post_instance, 'comments': comments, 'form': self.form_class,
-                       'reply_form': self.form_class_reply, 'can_like': can_like})
+                       'reply_form': self.form_class_reply})
 
     # @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
@@ -56,4 +53,4 @@ class PostDetailView(View):
             new_comment.post = self.post_instance
             new_comment.save()
             messages.success(request, 'your comment submitted successfully', 'success')
-            return redirect('home:post_detail', self.post_instance.id)
+            return redirect('blog:post_detail', self.post_instance.id)
